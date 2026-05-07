9bfc1f3dcc21e928cf59e8fb26b1bbb3034d802e
Avdhut Patole <avdhutpatole5556@gmail.com>
2026-05-07 20:32:22 +0530

    Added CreateTask file

---
+++ src/components/other/CreateTask.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const CreateTask = () => {
+  return (
+    
+      <div className="p-5 bg-[#1c1c1c] mt-7 rounded">
+        <form className=" flex flex-wrap w-full items-start justify-between">
+          <div className=" w-1/2">
+            <div>
+              <h3 className=" text-sm text-gray-300 mb-0.5">Task Title</h3>
+              <input type="text" className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border mb-4"/>
+            </div>
+            <div>
+              <h3 className=" text-sm text-gray-300 mb-0.5">Date</h3>
+              <input type="date" className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border mb-4"/>
+            </div>
+            <div>
+              <h3 className=" text-sm text-gray-300 mb-0.5">Assign to</h3>
+              <input type="text" className="text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border mb-4"/>
+            </div>
+            <div>
+              <h3 className=" text-sm text-gray-300 mb-0.5">Category</h3>
+              <input type="text" placeholder="Design, devlopment, etc" className="text-sm py-1 px-2 w-4/5 rounded mb-4 outline-none bg-transparent border" />
+            </div>
+          </div>
+
+          <div className=" w-2/5 flex flex-col items-start">
+            <h3 className=" text-sm text-gray-300 mb-0.5">Description</h3>
+            <textarea className=" w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border mb-4" name="" id="" placeholder="description"></textarea>
+            <button className=" bg-emerald-500 py-3 px-5 rounded hover:bg-emerald-600 text-sm w-full">Create Task</button>
+          </div>
+
+          
+        </form>
+      </div>
+    
+  )
+}
+
+export default CreateTask
