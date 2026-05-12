059b716573af1d0b15a2f3ec9852e341126e4209
Avdhut Patole <avdhutpatole5556@gmail.com>
2026-05-12 20:24:15 +0530

    Added & Fetch Data to the TaskList Component

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,53 +1,52 @@
 import React from 'react'
 
-const TaskList = () => {
+const TaskList = ({data}) => {
   return (
     
       <div id='Tasklist' className='overflow-x-auto flex items-center justify-start gap-5 flex-nowrap rounded-xl w-full h-[53%] mt-10'>
          <div className='h-full w-95 shrink-0 p-5 bg-red-400 rounded-xl'>
             <div className=' flex items-center justify-between'>
                 <h3 className=' bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
-                <h4 className='text-sm'>20 feb 2026</h4>
+                <h4 className='text-sm'>{data?.tasks?.[0]?.taskDate}</h4>
             </div>
-            <h2 className='text-3xl font-bold mt-7'>Make A Youtube Video</h2>
-            <p className='text-xm  mt-7'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, amet. Repellendus maiores est cumque autem?
+            <h2 className='text-3xl font-bold mt-7'>{data?.tasks?.[0]?.taskTitle}</h2>
+            <p className='text-xm  mt-7'>{data?.tasks?.[0]?.taskDescription}
             </p>
          </div>
         
         <div className='h-full w-95 shrink-0 p-5 bg-green-400 rounded-xl'>
             <div className=' flex items-center justify-between'>
                 <h3 className=' bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
-                <h4 className='text-sm'>20 feb 2026</h4>
+                <h4 className='text-sm'>{data?.tasks?.[1]?.taskDate}</h4>
             </div>
-            <h2 className='text-3xl font-bold mt-7'>Make A Youtube Video</h2>
-            <p className='text-xm  mt-7'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, amet. Repellendus maiores est cumque autem?
-            </p>
+            <h2 className='text-3xl font-bold mt-7'>{data?.tasks?.[1]?.taskTitle}</h2>
+            <p className='text-xm  mt-7'>{data?.tasks?.[1]?.taskDescription}</p>
          </div>
          <div className='h-full w-95 shrink-0 p-5 bg-blue-400 rounded-xl'>
             <div className=' flex items-center justify-between'>
                 <h3 className=' bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
-                <h4 className='text-sm'>20 feb 2026</h4>
+                <h4 className='text-sm'>{data?.tasks?.[2]?.taskDate}</h4>
             </div>
-            <h2 className='text-3xl font-bold mt-7'>Make A Youtube Video</h2>
-            <p className='text-xm  mt-7'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, amet. Repellendus maiores est cumque autem?
+            <h2 className='text-3xl font-bold mt-7'>{data?.tasks?.[2]?.taskTitle}</h2>
+            <p className='text-xm  mt-7'>{data?.tasks?.[2]?.taskDescription}
             </p>
          </div>
          <div className='h-full w-95 shrink-0 p-5 bg-yellow-400 rounded-xl'>
             <div className=' flex items-center justify-between'>
                 <h3 className=' bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
-                <h4 className='text-sm'>20 feb 2026</h4>
+                <h4 className='text-sm'>{data?.tasks?.[3]?.taskDate}</h4>
             </div>
-            <h2 className='text-3xl font-bold mt-7'>Make A Youtube Video</h2>
-            <p className='text-xm  mt-7'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, amet. Repellendus maiores est cumque autem?
+            <h2 className='text-3xl font-bold mt-7'>{data?.tasks?.[3]?.taskTitle}</h2>
+            <p className='text-xm  mt-7'>{data?.tasks?.[3]?.taskDescription}
             </p>
          </div>
          <div className='h-full w-95 shrink-0 p-5 bg-red-400 rounded-xl'>
             <div className=' flex items-center justify-between'>
                 <h3 className=' bg-red-600 px-3 py-1 text-sm rounded'>High</h3>
-                <h4 className='text-sm'>20 feb 2026</h4>
+                <h4 className='text-sm'>{data?.tasks?.[4]?.taskDate}</h4>
             </div>
-            <h2 className='text-3xl font-bold mt-7'>Make A Youtube Video</h2>
-            <p className='text-xm  mt-7'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequuntur, amet. Repellendus maiores est cumque autem?
+            <h2 className='text-3xl font-bold mt-7'>{data?.tasks?.[4]?.taskTitle}</h2>
+            <p className='text-xm  mt-7'>{data?.tasks?.[4]?.taskDescription}
             </p>
          </div>
         
