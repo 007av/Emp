a7cb1d119fe6099b99ca15174d0c010d934336ce
Avdhut Patole <avdhutpatole5556@gmail.com>
2026-05-14 12:32:14 +0530

    Added Logout feature for Admin & Employee

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -9,13 +9,21 @@ const Header = ({data}) => {
     if (data) {
       setUserName(data.firstName);
     }
+    else{
+      setUserName("Admin");
+    }
   }, [data]);
+
+  const handelUserLogout =()=>{
+    localStorage.setItem("loggedInUser", "");
+    window.location.reload(); //Automatically refresh the page after logout;
+  }
 
 
   return (
     <div className='flex items-end justify-between'>
       <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'> {userName} 👋</span></h1>
-      <button className='bg-red-600 px-5 py-2 rounded text-lg font-bold'>Log out</button>
+      <button className='bg-red-600 px-5 py-2 rounded text-lg font-bold' onClick={handelUserLogout}>Log out</button>
     </div>
   )
 }
